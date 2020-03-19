39745145acc4d02527e192b1c60978e2a1657887
eaajome <joshuabacuriomercado@gmail.com>
2020-03-19 23:39:30 +0100

    .

--- pages/projects.js
+++ pages/projects.js
@@ -1,36 +1,68 @@
-import projectService from "../services/projects.js";
 export default class ProjectsPage {
   constructor() {
     this.template();
-    this.initData();
+
   }
 
-  async initData() {
-    let projects = await projectService.loadProjects();
-    this.appendProjects(projects);
-  }
+
 
   template() {
     document.querySelector("#app").innerHTML += /*html*/ `
-        <section id="projects" class="page">
- 
- 
+       
+
+    <section id="projects" class="page">
+
+     <div class="card" style="width: 18rem; border:solid">
+      <img src="../images/teamplayer.png" class="card-img-top">
+        <div class="card-body">
+         <p class="card-text">TeamPlayer</p>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border:solid">
+      <img src="../images/teamplayer.png" class="card-img-top">
+        <div class="card-body">
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border:solid">
+      <img src="../images/teamplayer.png" class="card-img-top">
+        <div class="card-body">
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div class="card" style="width: 18rem; border:solid">
+      <img src="../images/teamplayer.png" class="card-img-top">
+        <div class="card-body">
+         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
         </section>
+
+
+
       `;
   }
 
-  appendProjects(projects) {
-    let template = "";
-    for (let project of projects) {
-      template += /*html*/ `
-
-   
-        `;
-    }
-    document.querySelector(".swiper-wrapper").innerHTML = template;
-  }
 
   // carousel
+  // async initData() {
+  //   let projects = await projectService.loadProjects();
+  //   this.appendProjects(projects);
+  // }
+
+  // appendProjects(projects) {
+  //   let template = "";
+  //   for (let project of projects) {
+  //     template += /*html*/ `
+
+
+  //       `;
+  //   }
+  //   document.querySelector("#projects").innerHTML = template;
+  // }
   //  <figure>
   //<img src="${project.img}">
   //<figcaption>${project.title}</figcaption>
