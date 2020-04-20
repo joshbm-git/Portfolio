8d2a45aeb71c06a21f014bc46b1314ec773dfbbf
eaajome <48531953+eaajome@users.noreply.github.com>
2020-04-20 15:22:38 +0200

    added captions

--- pages/projects.js
+++ pages/projects.js
@@ -7,7 +7,6 @@ export default class ProjectsPage {
     document.querySelector("#app").innerHTML += /*html*/ `
         <section id="projects" class="page">
 
-        <h2 class="year">2020</h2>
 
         <div class="book grid-container">
 
@@ -33,91 +32,129 @@ export default class ProjectsPage {
         data-link="https://joshbm.dk/projects/BoardGameFinder/">
         <img src="images/boardgamefinder.png" alt="" />
         </figure>
-        
-        <figure class="outer-figure" data-toggle="modal" data-target="#exampleModal" 
-        data-title="AeggetOgSkaegget" 
+    
+
+        <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
+        data-title="Ceres TeamPlayer" 
         data-caption="
-        <h2>AeggetOgSkaegget</h2>
-        <p>Do you want to eat good food that will make you full without having to make it?</p>
-        <p>Order at AeggetOgSkaegget. There a lot of food to choose from.</p>
-        <p>The best part is, everything is 55,-</p>
-        <ul>
-        <h3>Tools:</h3>
-        <li>HTML5</li>
-        <li>CSS3</li>
-        <li>SPA</li>
-        <li>API</li>
-        <li>ES6</li>
-        <li>Firebase</li>
-        </ul>
-        <p class='note'>Note: This was designed as a mobile app.</p"
-        data-img="images/aeggetogskaegget.png"
+        <p>Are you looking for an interesting fantasy football manager?</p>"
         data-caption-extra="
-        <h2>AeggetOgSkaegget</h2>
-        <p>Do you want to eat good food that will make you full without having to make it?</p>
-        <p>Order at AeggetOgSkaegget. There a lot of food to choose from.</p>
-        <p>The best part is, everything is 55,-</p>
+        <h2>TeamPlayer</h2>
+        <p>Are you looking for an interesting fantasy football manager?</p>
+        <p>TeamPlayer is a fantasy football manager app. Whenever you go to a football game, you can make a team consisting of the players from the 2 teams that are playing.</p>
+        <p>Every player gets points depening on their performance during that game. Your objective is to get the most points using who you think will make the most impact from both teams.</p>
         <ul>
         <h3>Tools:</h3>
         <li>HTML5</li>
         <li>CSS3</li>
-        <li>SPA</li>
-        <li>API</li>
-        <li>ES6</li>
-        <li>Firebase</li>
+        <li>JS</li>
         </ul>
-        <p class='note'>Note: This was designed as a mobile app.</p"
-        data-img="images/aeggetogskaegget.png">
-        <img src="images/aeggetogskaegget.png" alt="" />
+        <p>Second Semester</p>
+        <p class='note'>Note: This was designed as a mobile app.</p>"
+        data-img="images/teamplayer.png"
+        data-link="https://joshbm.dk/projects/BoardGameFinder/">
+        <img src="images/teamplayer.png" alt="" />
         </figure>
 
         <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
-        data-title="TeamPlayer" 
+        data-title="Eriksminde Efterskole" 
         data-caption="
-        <p>This project is a design for a football manager.</p>
-        <p>Functions: Burger Menu</p>"
-        data-img="images/thise.png">
+        <p>Are you having a tough time choosing an efterskole?</p>"
+        data-caption-extra="
+        <h2>Eriksminde Efterskole</h2>
+        <p>Eriksminde Efterskole is a place where we, with an artistic, democratic, professional and philosophical basis, make a school where people meet equally, safely and confidently.</p>
+        <p>We place the freedom, the appetite and the ability to be amazed and engrossed in the subjects, of life and of each other in the highest priority. We do our utmost to do everything we do, because it deserves life. And we do it together with a glance for the individual.</p>
+        <ul>
+        <h3>Tools:</h3>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JS</li>
+        </ul>
+        <p>Second Semester</p>"
+        data-img="images/webdoc.png"
+        data-link="https://joshbm.dk/projects/BoardGameFinder/">
+        <img src="images/webdoc.png" alt="" />
+        </figure>
+
+        <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
+        data-title="Thise greek yoghurt" 
+        data-caption="<p>Are you looking to lose weight without compromising the taste?</p>"
+        data-caption-extra="
+        <h2>Thise Light Greek Yoghurt</h2>
+        <p>Are you looking to lose weight without compromising the taste?</p>
+        <p>Thise is releasing a new greek yoghurt that has less fat, meaning less calories. It comes in different flavors.</p>
+        <p>You can also find recipes of your favorite food using this new greek yoghurt.</p>
+        <ul>
+        <h3>Tools:</h3>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>JS</li>
+        </ul>
+        <p>First Semester</p>"
+        data-img="images/thise.png"
+        data-link="https://joshbm.dk/projects/BoardGameFinder/">
+        <img src="images/thise.png" alt="" />
+        </figure>
+  
+
+            <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
+        data-title="Havnebadet" 
+        data-caption="<p>Are you looking for a perfect winter bathing spot?</p>"
+        data-caption-extra="
+        <h2>Havnebadet</h2>
+        <p>Are you looking for a perfect winter bathing spot?</p>
+        <p>Pack your swimming clothes, and jump in the water in 'Havnebadet' or The Harbour Bath. In the winter you can enjoy water, and get heated up in the saunas afterwards every saturday and sunday from 10-12.
+        <ul>
+        <h3>Tools:</h3>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        </ul>
+        <p>First Semester</p>"
+        data-img="images/havnebadet.png"
+        data-link="https://joshbm.dk/projects/BoardGameFinder/">
         <img src="images/thise.png" alt="" />
         </figure>
 
-
         <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
-        data-title="Thise" 
+        data-title="Asia Travel" 
         data-caption="
-        <p>This project is a design for a product.</p>
-        <p>Functions: Burger Menu</p>"
-        data-img="images/thise.png">
+        <p>Do you want to travel to Asia but don't know where to go and what to do?</p>"
+
+        data-caption-extra="
+        <p>Do you want to travel to Asia but don't know where to go and what to do?</p>
+        <p>Asia Travel provides guides to the most popular places to go in Asia. There are helpful tips such as how to plan your trip and what to do when arriving.</p>
+        <h3>Tools:</h3>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        </ul>        
+        <p>First Semester</p>"
+        data-img="images/thise.png"
+        data-link="https://joshbm.dk/projects/BoardGameFinder/">
         <img src="images/thise.png" alt="" />
         </figure>
 
         <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
         data-title="BJ Trading" 
         data-caption="
-        <p>This project is a design for a webshop, with a product page as the focus.</p>
-        <p>Functions: Burger Menu</p>"
-        data-img="images/thise.png" />'>
-        <img src="images/thise.png" alt="" />
+        <p>Are you itching to buy the newest electronics but don't have the dough?</p>"
+        data-caption-extra="
+        <h2>BJ Trading</h2>
+        <p>Are you itching to buy the newest electronics but don't have the dough?</p>
+        <p>BJ Trading has all the newest gadgets on the market, for the cheapest prices.</p>
+        <p>It's almost too good to be true!</p>
+        <ul>
+        <h3>Tools:</h3>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        </ul>
+        <p>First Semester</p>
+        <p class='note'>Note: This was only a product page.</p>"
+        data-img="images/teamplayer.png"
+        data-link="https://joshbm.dk/projects/BoardGameFinder/">
+        <img src="images/teamplayer.png" alt="" />
         </figure>
 
-        <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
-        data-title="Havnebadet" 
-        data-caption="
-        <p>This project is a design for an info page for a location.</p>
-        <p>Functions: Burger Menu</p>"
-        data-img="images/thise.png">
-        <img src="images/thise.png" alt="" />
-        </figure>
 
-        <figure class="outer-figure"  data-toggle="modal" data-target="#exampleModal" 
-        data-title="Online Magazine" 
-        data-caption="
-        <p>This project is a design for an online magazine/blog.</p>
-        <p>Functions: Burger Menu</p>"
-        data-img="images/thise.png">
-        <img src="images/thise.png" alt="" />
-        </figure>
-        
-        
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
